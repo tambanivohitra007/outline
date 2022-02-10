tambanivohitra007/outline

commit 5b062955512321475de8227e50860af32af08bd9

feat: Import improvements (#3064)
* feat: Split and simplify import/export pages in prep for more options

* minor fixes

* File operations for imports

* test

* icons

server/queues/processors/exports.ts
@@ -19,60 +19,62 @@ export default class ExportsProcessor {
         const user = await User.findByPk(actorId);
         invariant(user, "user operation not found");
 
-        const exportData = await FileOperation.findByPk(event.modelId);
-        invariant(exportData, "exportData not found");
+        const fileOperation = await FileOperation.findByPk(event.modelId);
+        invariant(fileOperation, "fileOperation not found");
 
         const collectionIds =
-          // @ts-expect-error ts-migrate(2339) FIXME: Property 'collectionId' does not exist on type 'Co... Remove this comment to see the full error message
-          event.collectionId || (await user.collectionIds());
+          "collectionId" in event
+            ? event.collectionId
+            : await user.collectionIds();
         const collections = await Collection.findAll({
           where: {
             id: collectionIds,
           },
         });
-        this.updateFileOperation(exportData, actorId, teamId, {
+        this.updateFileOperation(fileOperation, actorId, teamId, {
           state: "creating",
         });
         // heavy lifting of creating the zip file
         Logger.info(
           "processor",
-          `Archiving collections for file operation ${exportData.id}`
+          `Archiving collections for file operation ${fileOperation.id}`
         );
         const filePath = await archiveCollections(collections);
-        let url, state;
+        let url;
+        let state: any = "creating";
 
         try {
           // @ts-expect-error ts-migrate(2769) FIXME: No overload matches this call.
           const readBuffer = await fs.promises.readFile(filePath);
           // @ts-expect-error ts-migrate(2769) FIXME: No overload matches this call.
           const stat = await fs.promises.stat(filePath);
-          this.updateFileOperation(exportData, actorId, teamId, {
+          this.updateFileOperation(fileOperation, actorId, teamId, {
             state: "uploading",
             size: stat.size,
           });
           Logger.info(
             "processor",
-            `Uploading archive for file operation ${exportData.id}`
+            `Uploading archive for file operation ${fileOperation.id}`
           );
           url = await uploadToS3FromBuffer(
             readBuffer,
             "application/zip",
-            exportData.key,
+            fileOperation.key,
             "private"
           );
           Logger.info(
             "processor",
-            `Upload complete for file operation ${exportData.id}`
+            `Upload complete for file operation ${fileOperation.id}`
           );
           state = "complete";
         } catch (error) {
           Logger.error("Error exporting collection data", error, {
-            fileOperationId: exportData.id,
+            fileOperationId: fileOperation.id,
           });
           state = "error";
-          url = null;
+          url = undefined;
         } finally {
-          this.updateFileOperation(exportData, actorId, teamId, {
+          this.updateFileOperation(fileOperation, actorId, teamId, {
             state,
             url,
           });
@@ -85,7 +87,7 @@ export default class ExportsProcessor {
           } else {
             mailer.sendTemplate("exportSuccess", {
               to: user.email,
-              id: exportData.id,
+              id: fileOperation.id,
               teamUrl: team.url,
             });
           }
@@ -101,15 +103,14 @@ export default class ExportsProcessor {
     fileOperation: FileOperation,
     actorId: string,
     teamId: string,
-    data: Record<string, any>
+    data: Partial<FileOperation>
   ) {
     await fileOperation.update(data);
     await Event.add({
       name: "fileOperations.update",
       teamId,
       actorId,
-      // @ts-expect-error dataValues exists
-      data: fileOperation.dataValues,
+      modelId: fileOperation.id,
     });
   }
 }

app/components/DocumentViews.tsx
@@ -61,6 +61,7 @@ function DocumentViews({ document, isOpen }: Props) {
                 subtitle={subtitle}
                 image={<Avatar key={item.id} src={item.avatarUrl} size={32} />}
                 border={false}
+                compact
                 small
               />
             );

app/components/List/Item.tsx
@@ -10,6 +10,7 @@ type Props = {
   title: React.ReactNode;
   subtitle?: React.ReactNode;
   actions?: React.ReactNode;
+  compact?: boolean;
   border?: boolean;
   small?: boolean;
 };
@@ -49,6 +50,7 @@ const ListItem = (
       <Wrapper
         ref={ref}
         $border={border}
+        $compact={compact}
         activeStyle={{
           background: theme.primary,
         }}
@@ -62,16 +64,16 @@ const ListItem = (
   }
 
   return (
-    <Wrapper $border={border} {...rest}>
+    <Wrapper $compact={compact} $border={border} {...rest}>
       {content(false)}
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div<{ $border?: boolean }>`
+const Wrapper = styled.div<{ $compact?: boolean; $border?: boolean }>`
   display: flex;
-  padding: ${(props) => (props.$border === false ? 0 : "8px 0")};
-  margin: ${(props) => (props.$border === false ? "8px 0" : 0)};
+  margin: ${(props) => (props.$compact === false ? 0 : "8px 0")};
+  padding: ${(props) => (props.$compact === false ? "8px 0" : 0)};
   border-bottom: 1px solid
     ${(props) =>
       props.$border === false ? "transparent" : props.theme.divider};

app/components/Sidebar/Settings.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react";
 import {
-  DocumentIcon,
+  NewDocumentIcon,
   EmailIcon,
   ProfileIcon,
   PadlockIcon,
@@ -11,6 +11,7 @@ import {
   TeamIcon,
   ExpandedIcon,
   BeakerIcon,
+  DownloadIcon,
 } from "outline-icons";
 import * as React from "react";
 import { useTranslation } from "react-i18next";
@@ -118,11 +119,18 @@ function SettingsSidebar() {
               icon={<LinkIcon color="currentColor" />}
               label={t("Share Links")}
             />
+            {can.manage && (
+              <SidebarLink
+                to="/settings/import"
+                icon={<NewDocumentIcon color="currentColor" />}
+                label={t("Import")}
+              />
+            )}
             {can.export && (
               <SidebarLink
-                to="/settings/import-export"
-                icon={<DocumentIcon color="currentColor" />}
-                label={`${t("Import")} / ${t("Export")}`}
+                to="/settings/export"
+                icon={<DownloadIcon color="currentColor" />}
+                label={t("Export")}
               />
             )}
           </Section>

app/components/SocketProvider.tsx
@@ -328,15 +328,17 @@ class SocketProvider extends React.Component<Props> {
       }
     });
 
+    this.socket.on("fileOperations.create", async (event: any) => {
+      const user = auth.user;
+      if (user) {
+        fileOperations.add({ ...event, user });
+      }
+    });
+
     this.socket.on("fileOperations.update", async (event: any) => {
       const user = auth.user;
-      let collection = null;
-      if (event.collectionId) {
-        collection = await collections.fetch(event.collectionId);
-      }
-
       if (user) {
-        fileOperations.add({ ...event, user, collection });
+        fileOperations.add({ ...event, user });
       }
     });
 

app/components/Spinner.tsx
@@ -1,0 +1,59 @@
+import * as React from "react";
+import styled from "styled-components";
+
+export default function Spinner(props: React.HTMLAttributes<HTMLOrSVGElement>) {
+  return (
+    <SVG
+      width="16px"
+      height="16px"
+      viewBox="0 0 16 16"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <Circle
+        fill="none"
+        strokeWidth="2"
+        strokeLinecap="round"
+        cx="8"
+        cy="8"
+        r="6"
+      ></Circle>
+    </SVG>
+  );
+}
+
+const SVG = styled.svg`
+  @keyframes rotator {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(270deg);
+    }
+  }
+
+  animation: rotator 1.4s linear infinite;
+  margin: 4px;
+`;
+
+const Circle = styled.circle`
+  @keyframes dash {
+    0% {
+      stroke-dashoffset: 47;
+    }
+    50% {
+      stroke-dashoffset: 11;
+      transform: rotate(135deg);
+    }
+    100% {
+      stroke-dashoffset: 47;
+      transform: rotate(450deg);
+    }
+  }
+
+  stroke: ${(props) => props.theme.textSecondary};
+  stroke-dasharray: 46;
+  stroke-dashoffset: 0;
+  transform-origin: center;
+  animation: dash 1.4s ease-in-out infinite;
+`;

app/models/FileOperation.ts
@@ -1,6 +1,5 @@
 import { computed } from "mobx";
 import BaseModal from "./BaseModel";
-import Collection from "./Collection";
 import User from "./User";
 
 class FileOperation extends BaseModal {
@@ -8,11 +7,15 @@ class FileOperation extends BaseModal {
 
   state: string;
 
-  collection: Collection | null | undefined;
+  name: string;
+
+  error: string | null;
+
+  collectionId: string | null;
 
   size: number;
 
-  type: string;
+  type: "import" | "export";
 
   user: User;
 

app/routes/settings.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import { Switch, Redirect } from "react-router-dom";
 import Details from "~/scenes/Settings/Details";
+import Export from "~/scenes/Settings/Export";
 import Features from "~/scenes/Settings/Features";
 import Groups from "~/scenes/Settings/Groups";
-import ImportExport from "~/scenes/Settings/ImportExport";
+import Import from "~/scenes/Settings/Import";
 import Notifications from "~/scenes/Settings/Notifications";
 import People from "~/scenes/Settings/People";
 import Profile from "~/scenes/Settings/Profile";
@@ -33,7 +34,11 @@ export default function SettingsRoutes() {
       {isHosted && (
         <Route exact path="/settings/integrations/zapier" component={Zapier} />
       )}
-      <Route exact path="/settings/import-export" component={ImportExport} />
+      <Route exact path="/settings/import" component={Import} />
+      <Route exact path="/settings/export" component={Export} />
+
+      {/* old routes */}
+      <Redirect from="/settings/import-export" to="/settings/export" />
       <Redirect from="/settings/people" to="/settings/members" />
     </Switch>
   );

commit 0ade3eea42b9ee60736a1d19424c4aff7edd78aa

fix: Export collections produces empty zip, regressed in #3064

server/queues/processors/exports.ts
@@ -23,14 +23,16 @@ export default class ExportsProcessor {
         invariant(fileOperation, "fileOperation not found");
 
         const collectionIds =
-          "collectionId" in event
+          "collectionId" in event && event.collectionId
             ? event.collectionId
             : await user.collectionIds();
+
         const collections = await Collection.findAll({
           where: {
             id: collectionIds,
           },
         });
+
         this.updateFileOperation(fileOperation, actorId, teamId, {
           state: "creating",
         });