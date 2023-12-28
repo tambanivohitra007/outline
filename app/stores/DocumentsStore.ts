tambanivohitra007/outline

commit d61c9aea5c9b97775d1fe550c12f392d9864b768

Improve reliability of document operations with websocket disconnected

app/stores/DocumentsStore.ts
@@ -652,42 +652,6 @@ export default class DocumentsStore extends Store<Document> {
   }
 
   @action
-  async update(
-    params: {
-      id: string;
-      title?: string;
-      emoji?: string | null;
-      text?: string;
-      fullWidth?: boolean;
-      templateId?: string;
-    },
-    options?: {
-      publish?: boolean;
-      done?: boolean;
-      autosave?: boolean;
-    }
-  ) {
-    this.isSaving = true;
-
-    try {
-      const res = await client.post(`/${this.apiEndpoint}.update`, {
-        ...params,
-        ...options,
-        apiVersion: 2,
-      });
-
-      invariant(res?.data, "Data should be available");
-      this.addPolicies(res.policies);
-      const document = this.add(res.data.document);
-      const collection = this.getCollectionForDocument(document);
-      collection?.updateData(res.data.collection);
-      return document;
-    } finally {
-      this.isSaving = false;
-    }
-  }
-
-  @action
   async delete(
     document: Document,
     options?: {
@@ -750,18 +714,28 @@ export default class DocumentsStore extends Store<Document> {
   };
 
   @action
+  update = async (
+    params: Partial<Document>,
+    options?: Record<string, string | boolean | number | undefined>
+  ): Promise<Document> => {
+    const document = await super.update(params, options);
+    const collection = this.getCollectionForDocument(document);
+    void collection?.fetchDocuments({ force: true });
+    return document;
+  };
+
+  @action
   unpublish = async (document: Document) => {
     const res = await client.post("/documents.unpublish", {
       id: document.id,
-      apiVersion: 2,
     });
 
     runInAction("Document#unpublish", () => {
       invariant(res?.data, "Data should be available");
-      document.updateData(res.data.document);
-      const collection = this.getCollectionForDocument(document);
-      collection?.updateData(res.data.collection);
+      document.updateData(res.data);
       this.addPolicies(res.policies);
+      const collection = this.getCollectionForDocument(document);
+      void collection?.fetchDocuments({ force: true });
     });
   };
 

app/actions/definitions/documents.tsx
@@ -246,13 +246,17 @@ export const unpublishDocument = createAction({
       return;
     }
 
-    await document.unpublish();
+    try {
+      await document.unpublish();
 
-    toast.message(
-      t("Unpublished {{ documentName }}", {
-        documentName: document.noun,
-      })
-    );
+      toast.success(
+        t("Unpublished {{ documentName }}", {
+          documentName: document.noun,
+        })
+      );
+    } catch (err) {
+      toast.error(err.message);
+    }
   },
 });
 

server/routes/api/documents/documents.test.ts
@@ -3360,40 +3360,6 @@ describe("#documents.update", () => {
     expect(res.status).toBe(400);
     expect(body.message).toBe("id: Required");
   });
-
-  describe("apiVersion=2", () => {
-    it("should successfully publish a draft", async () => {
-      const team = await buildTeam();
-      const user = await buildUser({ teamId: team.id });
-      const collection = await buildCollection({
-        userId: user.id,
-        teamId: team.id,
-      });
-      const document = await buildDraftDocument({
-        title: "title",
-        text: "text",
-        teamId: team.id,
-        collectionId: null,
-      });
-
-      const res = await server.post("/api/documents.update", {
-        body: {
-          apiVersion: 2,
-          token: user.getJwtToken(),
-          id: document.id,
-          title: "Updated title",
-          text: "Updated text",
-          collectionId: collection.id,
-          publish: true,
-        },
-      });
-      const body = await res.json();
-      expect(res.status).toEqual(200);
-      expect(body.data.document.collectionId).toBe(collection.id);
-      expect(body.data.document.title).toBe("Updated title");
-      expect(body.data.document.text).toBe("Updated text");
-    });
-  });
 });
 
 describe("#documents.archive", () => {

commit f410146b3a509bceae5b68956fdc77f1f9a8d08a

fix: Flash in sidebar when publishing document (regression in d61c9aea5c9b97775d1fe550c12f392d9864b768)

app/stores/DocumentsStore.ts
@@ -707,10 +707,25 @@ export default class DocumentsStore extends Store<Document> {
     params: Partial<Document>,
     options?: Record<string, string | boolean | number | undefined>
   ): Promise<Document> {
-    const document = await super.update(params, options);
-    const collection = this.getCollectionForDocument(document);
-    void collection?.fetchDocuments({ force: true });
-    return document;
+    this.isSaving = true;
+
+    try {
+      const res = await client.post(`/${this.apiEndpoint}.update`, {
+        ...params,
+        ...options,
+      });
+      invariant(res?.data, "Data should be available");
+
+      const collection = this.getCollectionForDocument(res.data);
+      await collection?.fetchDocuments({ force: true });
+
+      const document = this.add(res.data);
+      this.addPolicies(res.policies);
+
+      return document;
+    } finally {
+      this.isSaving = false;
+    }
   }
 
   @action